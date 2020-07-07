35a6d70949a1891477e82a7d572887a843fe3781
sai vamsi yelamanchili <44221627+wonderfulvamsi@users.noreply.github.com>
2020-07-08 00:01:47 +0530

    Add files via upload

---
+++ online_resume.py
@@ -0,0 +1,46 @@
+import streamlit as st
+
+st.title("Sai Vamsi Yelamanchili")
+st.markdown("## :mortar_board: Student | :crystal_ball: AI entusiastic")
+st.markdown("######   :email: wonderfulvamsi@gmail.com   :telephone_receiver: +91 8886940907    :house_with_garden: Visakhapatnam, India    :computer: [GitHub profile](https://github.com/wonderfulvamsi) :newspaper: [Blogs](https://medium.com/@wonderingvamsi)  ")
+st.markdown(" --- ")
+st.image("https://avatars0.githubusercontent.com/u/44221627?s=400&u=951c62d2a170e022e94c0bcae0d24509c02e87d2&v=4", use_column_width=True)
+st.markdown(" --- ")
+
+st.header(":books: EDUCATION")
+st.markdown("##### ** Electronics and computer engineering **")
+st.write("Vignan institute of information technology (06/2018 – 04/2022)")
+st.markdown("###### 8.1 SGPA")
+
+st.header(":sunrise_over_mountains: EXPERIENCE")
+st.markdown("+ _ AI Hackathon 2019 (08/2019 – 09/2019) _ ")
+st.write(">Selected in one of the top 25 teams, out of 350+ teams nationwide for the second stage of the NVIDIA's AI Hackathon 2019.")
+st.write(">Served as team leader for our team.")
+st.markdown("+ _ On the plague trail: HackerEarth Machine Learning challenge (06/2019 – 06/2019) _")
+st.write(">Ranked in the top 100 out of 4836 participants. ")
+st.write(">Built a ML model with 82% accuracy when measured with RMSE")
+
+st.header(":rocket: PERSONAL PROJECTS")
+st.markdown("+ :coffee: _ [Star Rank](https://github.com/wonderfulvamsi/-My_1st_ML_Project) : ML model to rank the coffees of Starbucks (06/2019-07/2019)_")
+st.markdown("> Star Rank uses machine learning to rate all the coffees of Starbucks out of 5 stars, based on their nutrition levels.")
+st.write(">The model effectively segments all the coffees into 5 distinct classes by using the _ K-Means algorithm. _")
+
+st.markdown("+ :sa: _[Kannada handwritten digits recognition](https://github.com/wonderfulvamsi/Kannada-MNIST) (12/2019-12/2019)_ ")
+st.markdown("> Achieved 98% accuracy on training data & 72% accuracy on a complex validation data using Dense and Convolution Neural Networks.")
+
+st.markdown("+ :sparkles: _[Neural Style Transfer](https://github.com/wonderfulvamsi/Nerual-Style-Transfer-Web-App) (06/2020 – 06/2020)_")
+st.markdown("> Implemented neural style transfer using PyTorch & Streamlit.")
+
+st.markdown("+ :dizzy: _[Deep dream](https://github.com/wonderfulvamsi/DEEPDREAM) (06/2020 – 06/2020)_")
+st.markdown("> A web app to perform Deep dream on custom models.")
+
+st.markdown("+ :smiley: _[EmojiGAN](https://github.com/wonderfulvamsi/DCGAN) (06/2020 – 06/2020)_")
+st.markdown("> Trained a DCGAN to generate new emojis.")
+
+st.header(":seedling: SKILLS")
+st.markdown("+ Python programming\n\n + Automation\n\n+ SQL \n\n + Machine learning\n\n + Deep learning\n\n + Computer vision \n\n+ Data science\n\n + Predictive modeling \n\n+ Discriminative modeling \n\n + Generative modeling")
+
+st.header(":sparkling_heart: INTERESTS")
+st.markdown("+ STEM\n\n + Artificial intelligence\n\n+ Quantum computing \n\n + Deep reinforcemet learning\n\n + AGI\n\n + Robotics \n\n + Research")
+
+
